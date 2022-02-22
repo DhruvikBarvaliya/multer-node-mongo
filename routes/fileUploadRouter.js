4c0626813c54d7417dda6a6a8a243fe20f7989f3
Dhruvik <barvaliyadhruvik7@gmail.com>
2022-02-22 18:44:40 +0530

    changes for name

--- routes/fileUploadRouter.js
+++ routes/fileUploadRouter.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const { upload } = require('../helper.js/fileHelper')
+const { upload } = require('../helper/fileHelper')
 const { singleFileUpload, getallSingleFiles } = require('../controllers/fileUploadControler')
 
 router.post('/singleFile', upload.single('file'), singleFileUpload)
-router.get('/getSingleFiles', getallSingleFiles);
+router.get('/getAllSingleFiles', getallSingleFiles);
+router.get('/getSingleFiles/:name', getallSingleFiles);
 
 module.exports = { routes: router }
