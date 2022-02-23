388efd9278e1d53d20ccdc8c2bbacf240a9640fa
Dhruvik <barvaliyadhruvik7@gmail.com>
2022-02-23 19:03:13 +0530

    Change for get image

--- routes/fileUploadRouter.js
+++ routes/fileUploadRouter.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const { upload } = require('../helper.js/fileHelper')
+const { upload } = require('../helper/fileHelper')
 const { singleFileUpload, getallSingleFiles, getFile } = require('../controllers/fileUploadControler')
 
-router.post('/singleFile', upload.single('file'), singleFileUpload)
-router.get('/getSingleFiles', getallSingleFiles);
-router.get('/getFiles/:name', getFile);
+router.post('/addSingleFile', upload.single('file'), singleFileUpload)
+router.get('/getAllSingleFiles', getallSingleFiles);
+router.get('/getFileByName/:name', getFile);
 
 
 module.exports = { routes: router }
